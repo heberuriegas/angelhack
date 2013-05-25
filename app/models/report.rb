7b5a3e29d60d085bd3ae7d5d20bdce5a38dcbb03
Administrator <admin@local.host>
2013-05-25 18:06:08 -0500

    Gmaps4Rails geocoding
    Gmaps4Rails errores en geocoding resueltos en el modelo de reportes

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,12 +1,9 @@
 class Report < ActiveRecord::Base
   attr_accessible :content, :gmaps, :latitude, :longitude, :name
-  belongs_to :user
+  #belongs_to :user
 
-  acts_as_gmappable
+  acts_as_gmappable :process_geocoding => false
 
-def gmaps4rails_address
-  	address
-  end
 
   def gmaps4rails_infowindow
       "Problema: #{content}"
@@ -20,10 +17,5 @@ def gmaps4rails_address
    "marker_anchor" => [ 5, 10],
   }
   end
-
-  def gmaps4rails_sidebar
-  "<span>#{content}</span>" #put whatever you want here
-  end
-
 
 end
