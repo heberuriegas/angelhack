00572bf2911a47edb204d79302ecb999d595a2c6
Heber Garza <heber.fernando@gmail.com>
2013-05-25 13:54:15 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angelhack::Application.config.secret_token = 'f0c6b69c6236c56990b39bac26f873fe37436247e3425e4a6df55b1b121ae8ae363fc531909cc667ce383e7fbeac94c9a8df2530e7a3acda9ed7316bf6782822'
