546b9d44488c88d0ce10e6cbae08e606be707bc0
Administrator <admin@local.host>
2013-05-25 14:39:35 -0500

    User Model
    Validacion de campos : email, password, password_confirmation, crear un
    token privado

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,22 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :reports # dependent: :destroy
+  has_secure_password
+
+  before_save {|user| user.email = email.downcase}
+  before_save :create_remember_token
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+  			format: {with: VALID_EMAIL_REGEX},
+  			uniqueness: {case_sensitive:false}
+  validates :password, presence: true, lenght: {minimum: 6}
+  validates :password_confirmation, presence: true
+
+  def create_remember_token
+  	self.remember_me = SecureRandom.urlsafe_base64
+  end
+
 end
