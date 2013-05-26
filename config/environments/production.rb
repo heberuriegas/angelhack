4aa10e4c998e5f5551e66779454cf9aa21036c68
Heber Garza <heber.fernando@gmail.com>
2013-05-26 08:47:07 -0500

    Add precompile on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,5 @@ Angelhack::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.serve_static_assets = true
+  config.assets.initialize_on_precompile = false
 end
